0b12ef8d87edeb4456098eb9d7b99c73ee45a037
SrJorgeCruz <jorge_15243@hotmail.com>
2023-02-08 12:44:41 -0600

    Se agrega la boleta en la tabla y se cambian las longitudes de los varchar porque estaban muy cortas, además se agrega una prueba de inserción

--- CREACION DATABASE.sql
+++ CREACION DATABASE.sql
@@ -1,18 +1,19 @@
-Create database Sistema_Atencion_SS
+CREATE DATABASE Sistema_Atencion_SS
+GO
 USE Sistema_Atencion_SS
 GO
 DROP TABLE USERS
 GO
 CREATE TABLE USERS 
 (
-Id_user int primary key not null IDENTITY,
-boleta varchar not null,
-passw varbinary(max) not null,
-Tipo_user int, 
-Id_Estatus_user int,
-foreign key (Id_user) references DATA_USERS(Id_data_users),
-foreign key (Id_Estatus_user) references STATUS_USER(Id_Estatus_user),
-foreign key (Tipo_user) references TIPO_USERS_TABLE(Id_Tipo_Users) 
+Id_user INT PRIMARY KEY NOT NULL IDENTITY,
+boleta VARCHAR NOT NULL,
+passw VARBINARY(max) NOT NULL,
+Tipo_user INT, 
+Id_Estatus_user INT,
+FOREIGN KEY (Id_user) REFERENCES DATA_USERS(Id_data_users),
+FOREIGN KEY (Id_Estatus_user) REFERENCES STATUS_USER(Id_Estatus_user),
+FOREIGN KEY (Tipo_user) REFERENCES TIPO_USERS_TABLE(Id_Tipo_Users) 
 );
 GO
 CREATE TABLE STATUS_USER
@@ -23,60 +24,61 @@ DESCRIPICION_STATUS VARCHAR(10)
 GO
 CREATE TABLE DATA_USERS
 (
-Id_data_users int primary key not null,
-Nombre varchar(20),
-A_Materno varchar(20),
-A_Paterno varchar(20),
-CURP varchar(10),
-ID_SEXO int,
-ID_PLANTEL int, 
-CP int,
-tel_particular int,
-direccion varchar(max),
-alcaldia varchar(20),
-escolaridad varchar(20),
-correo  varchar(20),
-id_carrera INT,
-prestatario VARCHAR(20),
-codigo_Prestatario varchar(20),
-responsable VARCHAR(20),
-programa VARCHAR(20),
-clave_programa VARCHAR(20),
-cargo VARCHAR(20),
-tel_responsable VARCHAR(20),
-fecha_elaboracion date,
-fecha_inicio date,
-fecha_termino date,
-correo_prestatario varchar(20),
-ubicacion_calleynum varchar(20),
-ubicacion_colonia varchar(20),
-ubicacion_alcaldia varchar(20),
-ubicacion_codpos varchar(20),
-foreign key (id_carrera) references TABLE_CARRERA (id_carrera),
-foreign key (ID_SEXO) references TABLE_SEXO (Id_Sexo)
+Id_data_users INT PRIMARY KEY NOT NULL,
+Nombre VARCHAR(30),
+A_Paterno VARCHAR(30),
+A_Materno VARCHAR(30),
+CURP VARCHAR(22),
+Boleta VARCHAR(25),
+Id_Sexo INT,
+Id_Plantel int, 
+CP INT,
+Tel_particular VARCHAR(30),
+Direccion VARCHAR(max),
+Alcaldia VARCHAR(75),
+Escolaridad VARCHAR(10),
+Correo  VARCHAR(50),
+Id_carrera INT,
+Prestatario VARCHAR(150),
+Codigo_Prestatario VARCHAR(20),
+Responsable VARCHAR(75),
+Programa VARCHAR(150),
+Clave_programa VARCHAR(20),
+Cargo VARCHAR(55),
+Tel_responsable VARCHAR(50),
+Fecha_registro date,
+Fecha_inicio date,
+Fecha_termino date,
+Correo_prestatario VARCHAR(50),
+Ubicacion_calleynum VARCHAR(75),
+Ubicacion_colonia VARCHAR(75),
+Ubicacion_alcaldia VARCHAR(75),
+Ubicacion_codpos VARCHAR(75),
+FOREIGN KEY (Id_carrera) REFERENCES TABLE_CARRERA (id_carrera),
+FOREIGN KEY (Id_Sexo) REFERENCES TABLE_SEXO (Id_Sexo)
 );
 GO
-Create table TABLE_CARRERA
+CREATE TABLE TABLE_CARRERA
 (
-id_carrera int primary key not null,
-DESCPCION_CARRERARA VARCHAR(15)
+id_carrera INT PRIMARY KEY NOT NULL,
+DESCRIPCION_CARRERA VARCHAR(15)
 );
 GO
 ALTER TABLE TABLE_CARRERA
-ALTER COLUMN DESCPCION_CARRERARA VARCHAR(200)
+ALTER COLUMN DESCRIPCION_CARRERA VARCHAR(200)
 
 GO
 
-Create table TABLE_SEXO
+CREATE TABLE TABLE_SEXO
 (
-Id_Sexo int primary key not null,
+Id_Sexo INT PRIMARY KEY NOT NULL,
 NAME_SEXO VARCHAR(15)
 );
 GO
 
-Create table TIPO_USERS_TABLE
+CREATE TABLE TIPO_USERS_TABLE
 (
-Id_Tipo_Users int primary key not null,
-Descripcion_user varchar(20)
+Id_Tipo_Users INT PRIMARY KEY NOT NULL,
+Descripcion_user VARCHAR(20)
 )
 GO
---
+++ PruebaInsercion.sql
@@ -0,0 +1,29 @@
+INSERT INTO DATA_USERS (
+  Id_data_users, Nombre, A_Paterno, 
+  A_Materno, CURP, Boleta, Id_Sexo, 
+  Id_Plantel, CP, Tel_particular, Direccion, 
+  Alcaldia, Escolaridad, Correo, Id_carrera, 
+  Prestatario, Codigo_Prestatario, 
+  Responsable, Programa, Clave_programa, 
+  Cargo, Tel_responsable, Fecha_registro, 
+  Fecha_inicio, Fecha_termino, Correo_prestatario, 
+  Ubicacion_calleynum, Ubicacion_colonia, 
+  Ubicacion_alcaldia, Ubicacion_codpos
+) 
+VALUES 
+  (
+    1, 'JORGE ANGEL', 'CRUZ', 'MENESES', 
+    'CUMJ000723HMCRNRA8', '2019640027', 
+    1, 140, 55490, '5525263003', '1RA CERRADA DE IGNACIO ALDAMA Num. MZ.1 - LT.13, Col. MIGUEL HIDALGO', 
+    'ECATEPEC DE MORELOS, ESTADO DE MÉXICO.', 
+    '86 %', 'jcruzm1503@alumno.ipn.mx', 
+    1, 'ESCUELA SUPERIOR DE COMERCIO Y ADMINISTRACION, UNIDAD SANTO TOMAS', 
+    '100112200', 'C. GERARDO ENRIQUE ULLOA RODRIGUEZ', 
+    'ESCUELA SUPERIOR DE COMERCIO Y ADMINISTRACIÓN SANTO TOMÁS, SERVICIO SOCIAL EXTERNO (A DISTANCIA)', 
+    '05230a8308', 'RESPONSABLE DE INICIOS DE SERVICIO SOCIAL', 
+    '5523033575 61547', '2022-10-03', 
+    '2023-04-03', '2023-04-03', 'gulloar@ipn.mx', 
+    'PROLONGACIÓN DE CARPIO Num. 471 - SN.', 
+    'Col. PLUTARCO ELIAS CALLES', 'MIGUEL HIDALGO, CIUDAD DE MÉXICO.', 
+    'C.P. 11340'
+  );
