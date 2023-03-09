fa05858de869f21d493b483ae4e2eae45fd9593a
SrJorgeCruz <jorge_15243@hotmail.com>
2023-03-08 19:03:02 -0600

    Se agrega un archivo .py para agregar a los administradores del sistema, en este caso con Admin y contraseña del 1 al 6

---
+++ app/registro_admins.py
@@ -0,0 +1,56 @@
+from flask import Flask
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+import hashlib
+
+#server='DESKTOP-A8TJQDL\SQLEXPRESS01'  #PARA JOSHEP
+server='LAPTOP-9T4B4IDA' #PARA JORGE CRUZ
+#server='DANIEL\SQLEXPRESS' #PARA DANIEL
+bd='Sistema_Atencion_SS'
+user='SS_SISTEMAATENCION'
+password='Irvin19+'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://' + user + ':' + password + '@' + server + '/' + bd + '?driver=ODBC+Driver+17+for+SQL+Server'
+app.config['SESSION_TYPE'] = 'filesystem'
+app.secret_key = 'mysecretkey'
+Session(app)
+try:
+    db = SQLAlchemy(app)
+    print(" * Conexión a la base de datos exitosa")
+except Exception as e:
+    print("Error al conectarse a la base de datos: ", e)
+
+class Users(db.Model):
+    __tablename__ = 'USERS'
+    id = db.Column(db.Integer, primary_key=True, name='Id_user')
+    boleta = db.Column(db.String(25), nullable=True, name='boleta')
+    passw = db.Column(db.LargeBinary(), nullable=False,name = 'passw')
+    tipo_user = db.Column(db.Integer, nullable=False,name='Tipo_user')
+    id_status_user = db.Column(db.Integer, nullable=False, name='Id_Estatus_user')  
+
+def insertar_admin(data):
+    print(data)
+    password = data.get('contrasena')
+    passwo = hashlib.md5(password.encode('utf-8')).hexdigest().encode('utf-8')
+    print(password)
+    print(passwo)
+    try:
+        new_user = Users(
+            boleta = data.get('usuario'),
+            passw = passwo,
+            tipo_user = 1,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        print("Se insertó un nuevo administrador en la tabla USERS")
+    except Exception as e:
+        print("Error al insertar el administrador: ", e)
+        db.session.rollback()
+
+if __name__ == '__main__':
+    with app.app_context():
+        data = {'usuario': 'Admin', 'contrasena': '123456'}
+        insertar_admin(data)
+        print("La inserción se ha realizado correctamente")
+
